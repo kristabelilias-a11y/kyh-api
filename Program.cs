e42192e4870a5b0029c970c9e9b7b0267b7ab704
Kristabel Ilias <kristabel.ilias@hotmail.com>
2026-02-06 21:23:49 +0100

    fixade i program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using kyh_api.Models;
+using kyh_api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/encrypt", (Kryptering request) =>
+{
+    var encrypted = CaesarChiffer.Encrypt(request.Text, request.Shift);
+    return Results.Ok(new { original = request.Text, shift = request.Shift, encrypted });
+});
 
-app.Run();
+app.MapPost("/decrypt", (Kryptering request) =>
+{
+    var decrypted = CaesarChiffer.Decrypt(request.Text, request.Shift);
+    return Results.Ok(new { original = request.Text, shift = request.Shift, decrypted });
+});
+
+app.MapGet("/", () => "kyh-api is running!");
+
+app.Run(); 
